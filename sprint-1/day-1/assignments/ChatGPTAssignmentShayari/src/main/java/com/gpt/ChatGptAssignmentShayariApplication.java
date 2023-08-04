7313d5d6ddaeaa7ad07148a5015f127eb90bf383
ritikrana0169 <saviji520@gmail.com>
2023-08-04 21:51:11 +0530

    s

---
+++ sprint-1/day-1/assignments/ChatGPTAssignmentShayari/src/main/java/com/gpt/ChatGptAssignmentShayariApplication.java
@@ -0,0 +1,13 @@
+package com.gpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGptAssignmentShayariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatGptAssignmentShayariApplication.class, args);
+	}
+
+}
---
+++ sprint-1/day-1/assignments/ChatGPTAssignmentShayari/src/test/java/com/gpt/ChatGptAssignmentShayariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatGptAssignmentShayariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
