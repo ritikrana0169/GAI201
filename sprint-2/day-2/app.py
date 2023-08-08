33e8beb266f3cf6230937e00624f5dee84298786
ritikrana0169 <saviji520@gmail.com>
2023-08-08 09:54:37 +0530

    menu done

---
+++ sprint-2/day-2/app.py
@@ -0,0 +1,44 @@
+def main():
+    while True:
+        print("Welcome to Zesty Zomato!")
+        print("0. Display Menu")
+        print("1. Add Dish")
+        print("2. Remove Dish")
+        print("3. Update Dish Availability")
+        print("4. Take Order")
+        print("5. Update Order Status")
+        print("6. Review Orders")
+        print("7. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == '0':
+            # Implement displaying the menu
+            pass
+        elif choice == '1':
+            # Implement adding a new dish
+            pass
+        elif choice == '2':
+            # Implement removing a dish
+            pass
+        elif choice == '3':
+            # Implement updating dish availability
+            pass
+        elif choice == '4':
+            # Implement taking a new order
+            pass
+        elif choice == '5':
+            # Implement updating order status
+            pass
+        elif choice == '6':
+            # Implement reviewing orders
+            pass
+        elif choice == '7':
+            print("Thank you for using Zesty Zomato!")
+            break
+        else:
+            print("Invalid choice. Please select a valid option.")
+
+
+
+main()
